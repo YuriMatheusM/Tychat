9cfa2322212f367afac6f9024ddb0d84d450b00e
Yuri Matheus <yurimatheusmachadodossantos17@gmail.com>
2025-05-04 22:35:31 -0300

    first commit

---
+++ src/main/java/com/chat/v1/TychatApplication.java
@@ -0,0 +1,13 @@
+package com.chat.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TychatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TychatApplication.class, args);
+	}
+
+}
